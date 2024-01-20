6f9560831b170a2fc8464cdaeddabdc46bda5553
Hussein Aassem <hussienasem@gmail.com>
2024-01-20 19:04:26 +0200

    first commit

---
+++ src/main/java/com/social_feed/socialfeed/SocialFeedApplication.java
@@ -0,0 +1,13 @@
+package com.social_feed.socialfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialFeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social_feed/socialfeed/SocialFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social_feed.socialfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
